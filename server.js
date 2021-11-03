d78adba9071766e547045243b35e54ea34516553
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-11-02 21:07:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
+
+const app = express();
+
+app.use(express.json());
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.get('/heartbeat', (req, res) => {
+  res.render('hello world');
+})
+
+app.get('/', (req, res) => {
+  res.render('landing');
+})
+
+app.listen('8080', () => {
+  console.log('server is up and running!')
+})
