f115e9faf421676f539343d32e886b487f12bc43
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-11-05 21:52:23 -0400

    adds show flashcards and mock seeded data, starts filter flashcards

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
+const Sequelize = require('sequelize');
+const { Flashcard } = require('./models');
 
 const app = express();
 
+app.use('/', express.static(__dirname + '/public'));
 app.use(express.json());
 
 // setup the template engine
@@ -12,12 +15,116 @@ app.set('views', 'views');
 app.set('view engine', 'html');
 
 app.get('/heartbeat', (req, res) => {
-  res.render('hello world');
+  res.json({
+    "is": "working"
+  })
 })
 
 app.get('/', (req, res) => {
   res.render('landing');
+});
+
+// display all flashcards
+app.get('/show/all', async (req, res) => {
+  const flashcards = await Flashcard.findAll();
+
+  const html = flashcards
+  .map((flashcard) => {
+    return `
+    <div class="accordion mx-auto" id="accordion" style="width: 75%">
+    <div class="card mb-5">
+      <div class="card-header" id="heading${flashcard.id}">
+        <h4>${flashcard.category}</h4>
+        <p>${flashcard.question}</p>
+        <h2 class="mb-0">
+          <button
+            class="btn btn-dark"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapse${flashcard.id}"
+            aria-expanded="true"
+            aria-controls="collapse${flashcard.id}"
+          > 
+            Answer
+          </button>
+        </h2>
+      </div>
+      <div
+        id="collapse${flashcard.id}" 
+        class="collapse"
+        aria-labelledby="heading${flashcard.id}"
+        data-parent="#accordion"
+      >
+        <div class="card-body">
+          ${flashcard.answer}
+        </div>
+      </div>
+    </div>  
+  </div>
+    `
+  })
+  .join('')
+  
+  res.render('flashcard-list', {
+    locals: {
+      flashcards: html
+    }
+  });
+});
+
+// filter flashcards by category
+app.get('/show/:category', async (req, res) => {
+  const flashcards = await Flashcard.findAll({
+    where: {
+      category: req.params.category
+    }
+  });
+
+  const html = flashcards
+  .map((flashcard) => {
+    return `
+    <div class="accordion mx-auto" id="accordion" style="width: 75%">
+    <div class="card mb-5">
+      <div class="card-header" id="heading${flashcard.id}">
+        <h4>${flashcard.category}</h4>
+        <p>${flashcard.question}</p>
+        <h2 class="mb-0">
+          <button
+            class="btn btn-dark"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapse${flashcard.id}"
+            aria-expanded="true"
+            aria-controls="collapse${flashcard.id}"
+          > 
+            Answer
+          </button>
+        </h2>
+      </div>
+      <div
+        id="collapse${flashcard.id}" 
+        class="collapse"
+        aria-labelledby="heading${flashcard.id}"
+        data-parent="#accordion"
+      >
+        <div class="card-body">
+          ${flashcard.answer}
+        </div>
+      </div>
+    </div>  
+  </div>
+    `
+  })
+  .join('')
+  
+  res.render('flashcard-list', {
+    locals: {
+      flashcards: html
+    }
+  });
+
 })
+
 
 app.listen('8080', () => {
   console.log('server is up and running!')
